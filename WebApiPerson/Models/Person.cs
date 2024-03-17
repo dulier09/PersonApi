e9337272099c8943f0683e8b7f351e7a3ed7fa60
dulier09 <dulieram@gmail.com>
2024-03-17 17:08:14 -0500

    Condicion para el salario

---
+++ WebApiPerson/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApiPerson.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public required string LastName { get; set; }
+        public string SecondLastName { get; set; }
+        public required DateTime Birthdate { get; set; }
+        public required double Salary { get; set; }
+        public required DateTime CreationDate { get; set;}
+        public required DateTime UpdateDate { get; set;}
+
+    }
+}
